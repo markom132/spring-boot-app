c887bc28e8a58f4321618439c130157047cea5dd
Marko Markovic <106665307+markom132@users.noreply.github.com>
2024-02-05 14:09:44 +0100

    Update BalloonApplication.java

--- src/main/java/com/example/demo/BalloonApplication.java
+++ src/main/java/com/example/demo/BalloonApplication.java
@@ -6,6 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class BalloonApplication {
 
+
+	
 	public static void main(String[] args) {
 		SpringApplication.run(BalloonApplication.class, args);
 	}
