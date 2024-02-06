875e252c816d0b7fd628be7be214a2b2104d8217
Marko Markovic <106665307+markom132@users.noreply.github.com>
2024-02-06 11:57:38 +0100

    Update BalloonApplication.java

--- src/main/java/com/example/demo/BalloonApplication.java
+++ src/main/java/com/example/demo/BalloonApplication.java
@@ -29,4 +29,86 @@ public class Calculator {
         int result = calculator.complexCalculation(2, 3, 4);
         System.out.println("Result: " + result);
     }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    public int complexCalculat1ion(int x, int y, int z) {
+        int sum = add(x, y);
+        int product = multiply(sum, z);
+
+        // Intentionally introduce complexity by adding unnecessary code
+        for (int i = 0; i < 10; i++) {
+            if (i % 2 == 0) {
+                product += i;
+            } else {
+                product -= i;
+            }
+        }
+
+        return product;
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+    
 }
