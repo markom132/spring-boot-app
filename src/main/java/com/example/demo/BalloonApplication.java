ff3a6b77562172e96f2658ab272da43bc7be05fb
Marko Markovic <106665307+markom132@users.noreply.github.com>
2024-02-06 11:43:16 +0100

    Update BalloonApplication.java

--- src/main/java/com/example/demo/BalloonApplication.java
+++ src/main/java/com/example/demo/BalloonApplication.java
@@ -9,7 +9,21 @@ public class BalloonApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(BalloonApplication.class, args);
 	}
+    public int complexCalculation(int x, int y, int z) {
+        int sum = add(x, y);
+        int product = multiply(sum, z);
 
+        // Intentionally introduce complexity by adding unnecessary code
+        for (int i = 0; i < 10; i++) {
+            if (i % 2 == 0) {
+                product += i;
+            } else {
+                product -= i;
+            }
+        }
+
+        return product;
+    }
 }
 
 
